adbb426407bf72143b5ee47f42c7345f9e07c01c
ROFI <rodrigofigueroa.name@gmail.com>
2025-03-05 07:07:31 -0300

    removed ai dir

--- expo-ai-chatbot/src/components/ai/core/generate-object/index.ts
+++
@@ -1,7 +0,0 @@
-export { generateObject, experimental_generateObject } from './generate-object';
-export type { GenerateObjectResult } from './generate-object-result';
-export { streamObject, experimental_streamObject } from './stream-object';
-export type {
-  ObjectStreamPart,
-  StreamObjectResult,
-} from './stream-object-result';
--- expo-ai-chatbot/src/components/ai/util/delay.ts
+++
@@ -1,5 +0,0 @@
-export async function delay(delayInMs?: number): Promise<void> {
-  return delayInMs === undefined
-    ? Promise.resolve()
-    : new Promise(resolve => setTimeout(resolve, delayInMs));
-}
